0b9c74e939aedd2e8bb57ee7ff56ebd26dbd3286
chanchal rajput <nsthakur797909@gmail.com>
2026-08-16 07:58:01 +0530

    handling authentication pop up in playwright

---
+++ tests/auth.spec.ts
@@ -0,0 +1,24 @@
+import { test ,expect,Browser,Page,Locator, BrowserContext} from '@playwright/test'; 
+   
+import {webkit,chromium,firefox} from '@playwright/test';
+
+test('auth test',async()=>{
+    const browser:Browser =await chromium.launch({headless:false ,channel:'chrome'});
+    const  context:BrowserContext=await browser.newContext();
+    const page:Page=await context.newPage();
+
+
+
+const username='admin';
+const password='admin';
+const authHeader='basic' + btoa(username+password);
+page.setExtraHTTPHeaders({Authorization:authHeader});
+
+// pass and username directly link m n use krke can be used like this  by initialization
+
+    await page.goto('https://the-internet.herokuapp.com/basis_auth');
+    
+
+
+    // await new Promise (()=>{});
+})
